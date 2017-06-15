c479280dc2b4333c34555aaa013a3a13b5745a0e
DrewRoberts <drew@roberts.email>
2017-06-15 15:02:53 -0500

    Edited Lex

--- resources/views/amp/lexington.blade.php
+++ resources/views/amp/lexington.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="utf-8">
     <script async src="https://cdn.ampproject.org/v0.js"></script>
-    <title>Lexington Breakout Games</title>
+    <title>Lexington Escape Room | Breakout Games</title>
     <link rel="canonical" href="https://drewroberts.com/lexington">
+    <link href="https://fonts.googleapis.com/css?family=Goudy+Bookletter+1911" rel="stylesheet">
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
     <style amp-custom>
@@ -16,20 +17,6 @@
             font-size: 17px;
             font-weight: 700;
             line-height: 1.65em;
-        }
-        section, header, footer {
-            width: 90%;
-            display: block;
-            margin: 30px 5%;
-        }
-        header, footer {
-            width: 90%;
-            margin: 0;
-            padding: 0 5%;
-            background-color: #0D0D0D;
-        }
-        header {
-            height: 65px;
         }
         h1, h2, h3 {
             font-family: Baskerville, "Palatino Linotype", Palatino, "Century Schoolbook L", "Times New Roman", "serif";
@@ -44,29 +31,40 @@
         }
         .button {
             background-color: #E63C2F;
-            padding: 12px 20px;
+            padding: 15px 20px;
             color: #E5E4DA;
             text-decoration: none;
-            display: block;
-            width: auto;
             font-size: 15px;
             font-weight: 300;
         }
         hr {
-            margin-top: 20px;
+            margin-top: 80px;
             margin-bottom: 20px;
             border-color: #E63C2F;
             border-top: 1px solid #E5E4DA;
         }
+        section, header, footer {
+            width: 90%;
+            display: block;
+            margin: 30px 5%;
+        }
+        header, footer {
+            width: 90%;
+            margin: 0;
+            padding: 0 5%;
+            background-color: #0D0D0D;
+        }
+        header {
+            height: 80px;
+        }
         header amp-img.logo {
-            max-width: 180px;
             margin-top: 20px;
         }
         header .button {
             float: right;
             width: auto;
             padding: 8px 15px;
-            margin-top: 10px;
+            margin-top: 17px;
         }
         section.hero {
             width: 100%;
@@ -74,9 +72,6 @@
             padding: 0;
         }
         section.hero h1 {
-            position: absolute;
-            top: 70px;
-            left: 5%;
             font-size: 50px;
             font-style: italic;
             line-height: 80px;
@@ -105,8 +100,8 @@
         .game-room {
             padding: 30px 0;
         }
-        .game-room amp-img.icon {
-            max-width: 100px;
+        .game-room amp-youtube {
+            margin-bottom: 30px;
         }
         .game-room .button {
             margin-top: 20px;
@@ -130,8 +125,14 @@
             text-decoration: none;
             color: #0D0D0D;
         }
+        section.location img {
+            display: block;
+            max-width: 100%;
+        }
         footer {
             padding-top: 50px;
+            padding-bottom: 40px;
+            margin-bottom: 0;
         }
         footer img.logo {
             max-width: 80px;
@@ -157,22 +158,22 @@
             color: #E5E4DA;
             font-weight: 300;
             font-size: 15px;
+            display: inline;
         }
     </style>
 </head>
 <body>
     <script async custom-element="amp-youtube" src="https://cdn.ampproject.org/v0/amp-youtube-0.1.js"></script>
-
+    
     <header>
-        <a href="https://breakoutgames.com/lexington/">
-            <amp-img class="logo"
-                alt="Breakout Games"
-                src="https://2m14st1h0x1b30k8dw3a8fxi-wpengine.netdna-ssl.com/wp-content/uploads/2016/07/Breakout-Logo.svg"
-                width="300"
-                height="37"
-                layout="fixed">
-            </amp-img>
-        </a> 
+
+        <amp-img class="logo"
+            alt="Breakout Games"
+            src="https://2m14st1h0x1b30k8dw3a8fxi-wpengine.netdna-ssl.com/wp-content/uploads/2016/07/Breakout-Logo.svg"
+            width="180"
+            height= "37"
+            layout="fixed">
+        </amp-img>       
         <a class="button" href="https://checkout.breakoutgames.com/listing?location=3154P9J9J71491A95C747">Play Now</a>
     </header>
 
@@ -183,7 +184,7 @@
             width="600"
             height="282"
             layout="responsive">
-        </amp-img>
+        </amp-img>    
     </section>
 
     <section class="intro">
@@ -213,7 +214,7 @@
               data-videoid="uaaUv7PCjnc">
             </amp-youtube>              
 
-            <a class="button" href="https://breakoutgames.com/book-at-lexington/">Play at Lexington</a>
+            <a class="button" href="https://checkout.breakoutgames.com/listing?location=3154P9J9J71491A95C747">Play at Lexington</a>
             <hr>
         </div>
 
@@ -234,7 +235,7 @@
               data-videoid="ZaK7eXNOu6A">
             </amp-youtube>              
 
-            <a class="button" href="https://breakoutgames.com/book-at-lexington/">Play at Lexington</a>
+            <a class="button" href="https://checkout.breakoutgames.com/listing?location=3154P9J9J71491A95C747">Play at Lexington</a>
             <hr>
         </div>
 
@@ -250,13 +251,13 @@
             <p>A famous Veterinarian is on the loose after he is caught drugging racehorses. In a desperate effort to exact revenge he sneaks into the Derby and steals the Trophy, the $2 Million Purse and the Blanket of Roses. The Derby starts in one hour and your team must sneak into his house, recover all three items, and escape before it begins.</p>
 
             <amp-img alt="Derby Heist"
-                src="https://2m14st1h0x1b30k8dw3a8fxi.wpengine.netdna-cdn.com/wp-content/uploads/2016/06/derby_heist-1.jpg"
-                width="1920"
-                height="1080"
+                src="https://drewroberts.com/derby_heist.jpg"
+                width="600"
+                height="338"
                 layout="responsive">
             </amp-img>
 
-            <a class="button" href="https://breakoutgames.com/book-at-lexington/">Play at Lexington</a>
+            <a class="button" href="https://checkout.breakoutgames.com/listing?location=3154P9J9J71491A95C747">Play at Lexington</a>
             <hr>
         </div>           
 
@@ -277,7 +278,7 @@
               data-videoid="jekUvwNxNPo">
             </amp-youtube>              
 
-            <a class="button" href="https://breakoutgames.com/book-at-lexington/">Play at Lexington</a>
+            <a class="button" href="https://checkout.breakoutgames.com/listing?location=3154P9J9J71491A95C747">Play at Lexington</a>
             <hr>
         </div>                       
 
@@ -298,7 +299,7 @@
               data-videoid="p8LuxQRIOhs">
             </amp-youtube>              
 
-            <a class="button" href="https://breakoutgames.com/book-at-lexington/">Play at Lexington</a>
+            <a class="button" href="https://checkout.breakoutgames.com/listing?location=3154P9J9J71491A95C747">Play at Lexington</a>
             <hr>
         </div>                       
 
@@ -319,7 +320,7 @@
               data-videoid="JOpURcCS800">
             </amp-youtube>              
 
-            <a class="button" href="https://breakoutgames.com/book-at-lexington/">Play at Lexington</a>
+            <a class="button" href="https://checkout.breakoutgames.com/listing?location=3154P9J9J71491A95C747">Play at Lexington</a>
             <hr>
         </div>
     </section>         
@@ -333,34 +334,24 @@
         <ul>
             <li><a href="https://checkout.breakoutgames.com/listing?location=3154P9J9J71491A95C747">View Game Times</a></li>
             <li><a href="https://goo.gl/maps/kRkKo7konC12" target="_blank">Get Directions</a></li>
-            <li><a data-toggle="modal" data-target="#locNewsletterModal_357" data-dismiss="modal">Sign Up for Updates</a></li>
+            <li><a href="https://breakoutgames.com/subscribe">Sign Up for Updates</a></li>
         </ul>
-        <div id="map" class="m-map embed-responsive embed-responsive-1by1" data-map="[{&quot;location&quot;:&quot;Lexington&quot;,&quot;latitude&quot;:&quot;38.038902&quot;,&quot;longitude&quot;:&quot;-84.478457&quot;}]"></div>
+        <a href="https://www.google.com/maps/place/Breakout+Games/@38.038902,-84.478457,15z/data=!4m5!3m4!1s0x0:0xce55ff8646ee1026!8m2!3d38.0389025!4d-84.4784575?hl=en-US&shorturl=1"><img src="https://drewroberts.com/lexington-map.jpg" /></a>
     </section>         
 
     <footer>
-        <amp-img class="logo"
-            alt="Breakout Games Escape Rooms"
-            src="https://2m14st1h0x1b30k8dw3a8fxi-wpengine.netdna-ssl.com/wp-content/uploads/2016/04/ftr-logo.svg"
-            width="149"
-            height="150"
-            layout="fixed">
-        </amp-img>
-        <ul>
-            <li><a title="Team Building" href="https://breakoutgames.com/team-building/">Team Building</a></li>
-            <li><a title="Frequently Asked Questions" href="https://breakoutgames.com/faq-lexington/">Frequently Asked Questions</a></li>
-            <li><a title="Fill Out Waiver" href="https://breakoutgames.com/lexington-waiver/">Fill Out Waiver</a></li>
-            <li><a title="Gift Vouchers" href="https://breakoutgames.com/lexington-voucher/">Gift Vouchers</a></li>
-            <li><a title="Blog - Into the Maze" href="https://breakoutgames.com/blog/">Blog &#8211; Into the Maze</a></li>
-            <li><a title="Join our Team" href="https://breakoutgames.com/join-our-lexington-team/">Join our Team</a></li>
-            <li><a title="Contact Us" href="https://breakoutgames.com/contact-us/">Contact Us</a></li>
-            <li><a title="What Are Escape Rooms" href="https://breakoutgames.com/escape-rooms/">What are escape rooms?</a></li>
-        </ul>        
-        <!--<p>Follow Us!</p>
-        <a target="_blank" href="https://www.facebook.com/breakoutgameslexington"><i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i></a>
-        <a target="_blank" href="https://www.instagram.com/breakoutlexington/"><i class="fa fa-instagram fa-3x" aria-hidden="true"></i></a>
-        <a target="_blank" href="https://twitter.com/breakoutlex"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>-->
-        <p>&copy; copyright Breakout, 2017.</p>
+    <img class="logo" src="https://2m14st1h0x1b30k8dw3a8fxi-wpengine.netdna-ssl.com/wp-content/uploads/2016/04/ftr-logo.svg"/>
+    <ul>
+        <li><a title="Team Building" href="https://breakoutgames.com/team-building/">Team Building</a></li>
+        <li><a title="Frequently Asked Questions" href="https://breakoutgames.com/faq-lexington/">Frequently Asked Questions</a></li>
+        <li><a title="Fill Out Waiver" href="https://breakoutgames.com/lexington-waiver/">Fill Out Waiver</a></li>
+        <li><a title="Gift Vouchers" href="https://breakoutgames.com/lexington-voucher/">Gift Vouchers</a></li>
+        <li><a title="Blog - Into the Maze" href="https://breakoutgames.com/blog/">Blog &#8211; Into the Maze</a></li>
+        <li><a title="Join our Team" href="https://breakoutgames.com/join-our-lexington-team/">Join our Team</a></li>
+        <li><a title="Contact Us" href="https://breakoutgames.com/contact-us/">Contact Us</a></li>
+        <li><a title="What Are Escape Rooms" href="https://breakoutgames.com/escape-rooms/">What are escape rooms?</a></li>
+    </ul>        
+    <p>&copy; copyright Breakout, 2017.</p>
     </footer>                
                         
     
