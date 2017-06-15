2b16c22a750c14c365a664b26ce62b49909d2dae
DrewRoberts <drew@roberts.email>
2017-06-15 12:52:22 -0500

    Edited Lex

--- resources/views/amp/lexington.blade.php
+++ resources/views/amp/lexington.blade.php
@@ -14,16 +14,153 @@
 </head>
 <body>
 <script async custom-element="amp-youtube" src="https://cdn.ampproject.org/v0/amp-youtube-0.1.js"></script>
-<amp-img src="http://breakoutstaging.mediocrecreative.com/breakout/wp-content/themes/fm-framework/images/location_header_background_3.jpg"
-  width="1200"
-  height="536"
-  layout="responsive"></amp-img>
-<h1>Lexington</h1>
-<div style="max-width: 480;">
-<amp-youtube width="480"
-  height="270"
-  layout="responsive"
-  data-videoid="uaaUv7PCjnc">
-</amp-youtube>
-</div>
+    <header>
+        <a href="https://breakoutgames.com/lexington/"><img src="https://2m14st1h0x1b30k8dw3a8fxi-wpengine.netdna-ssl.com/wp-content/uploads/2016/07/Breakout-Logo.svg"/></a> 
+        <a href="https://checkout.breakoutgames.com/listing?location=3154P9J9J71491A95C747">Play Now</a>
+        <i class="fa fa-map-marker"></i><span>KY - Lexington</span>
+        <i class="fa fa-map-marker"></i>Your Location: <span>Lexington</span>
+    </header>
+
+    <section class="hero">
+        <h1>Open for Adventure</h1>
+    </section>
+
+    <section class="intro">
+        <h1>Breakout Lexington</h1>
+        <p>This isn’t your average outing or everyday experience—Breakout is for those who would rather solve mysteries than watch someone else have all the fun. With different rooms that follow unique storylines, you and your friends will have 60 minutes to escape your adventure of choice by cracking codes, solving puzzles and following clues. Part problem solving, part adrenaline, end-to-end fun.</p>
+
+        <h3>Breakout Brief</h3>
+        <p>A Life-Sized Game of Twists &amp; Turns</p>
+        <a href="#">Launch Video Briefing</a>
+    </section>         
+
+
+    <section class="games">
+        <div class="game-room">
+            <img class="icon" src="https://2m14st1h0x1b30k8dw3a8fxi-wpengine.netdna-ssl.com/wp-content/uploads/2016/09/mysterymansion.svg" />
+            <h1>Mystery Mansion</h1>
+            <p>This spine-chilling adventure starts in the ruins of an old mansion – abandoned decades ago by a reclusive and eccentric family. Since then, rumors have spread about a presence looming over the old house. Work as a team to race the clock and uncover the mansion's secrets in this all-ages mystery adventure.</p>
+
+            <amp-youtube width="480"
+              height="270"
+              layout="responsive"
+              data-videoid="uaaUv7PCjnc">
+            </amp-youtube>              
+
+            <a class="button" href="https://breakoutgames.com/book-at-lexington/">Play at Lexington</a>
+            <hr>
+        </div>
+
+        <div class="game-room">
+            <img class="icon" src="https://2m14st1h0x1b30k8dw3a8fxi-wpengine.netdna-ssl.com/wp-content/uploads/2016/05/kidnapping.svg" />
+            <h1>The Kidnapping</h1>
+            <p>The kidnapping begins with you and your fellow captives waking up handcuffed and blindfolded in a strange room. With nothing but collective brainpower, hidden clues and 60 minutes on your side, you have to free yourselves before the kidnapper returns. A heart-pumping thriller from start to finish, you can expect a rush of non-stop fun.</p>
+
+            <amp-youtube width="480"
+              height="270"
+              layout="responsive"
+              data-videoid="uaaUv7PCjnc">
+            </amp-youtube>              
+
+            <a class="button" href="https://breakoutgames.com/book-at-lexington/">Play at Lexington</a>
+            <hr>
+        </div>
+
+        <div class="game-room">
+            <img class="icon" src="https://2m14st1h0x1b30k8dw3a8fxi-wpengine.netdna-ssl.com/wp-content/uploads/2016/06/derby.svg" />
+            <h1>Derby Heist</h1>
+            <p>A famous Veterinarian is on the loose after he is caught drugging racehorses. In a desperate effort to exact revenge he sneaks into the Derby and steals the Trophy, the $2 Million Purse and the Blanket of Roses. The Derby starts in one hour and your team must sneak into his house, recover all three items, and escape before it begins.</p>
+
+            <amp-youtube width="480"
+              height="270"
+              layout="responsive"
+              data-videoid="uaaUv7PCjnc">
+            </amp-youtube>              
+
+            <a class="button" href="https://breakoutgames.com/book-at-lexington/">Play at Lexington</a>
+            <hr>
+        </div>           
+
+        <div class="game-room">
+            <img class="icon" src="https://2m14st1h0x1b30k8dw3a8fxi-wpengine.netdna-ssl.com/wp-content/uploads/2016/05/casino-royale.svg" />
+            <h1>Operation: Casino</h1>
+            <p>In this life-sized spy game, you will be sent on a trail of clues to discover the alias of a missing agent, and bring down the syndicate responsible for his disappearance. Follow unexpected twists and turns, break codes and channel your alter-ego secret agent to escape the room in an hour or less. If you’re down with espionage, this is your game.</p>
+
+            <amp-youtube width="480"
+              height="270"
+              layout="responsive"
+              data-videoid="uaaUv7PCjnc">
+            </amp-youtube>              
+
+            <a class="button" href="https://breakoutgames.com/book-at-lexington/">Play at Lexington</a>
+            <hr>
+        </div>                       
+
+        <div class="game-room">
+            <img class="icon" src="https://2m14st1h0x1b30k8dw3a8fxi-wpengine.netdna-ssl.com/wp-content/uploads/2016/04/island-escape.svg" />
+            <h1>Island Escape</h1>
+            <p>While vacationing on a tropical island, you awake to the sounds of a dormant volcano rumbling to life. The only people still on the island, you and your team are left to your own devices and must work together to discover the keys to safety before the lava reaches shore. Bring your inner explorer out to play for this adrenaline-packed adventure.</p>
+
+            <amp-youtube width="480"
+              height="270"
+              layout="responsive"
+              data-videoid="uaaUv7PCjnc">
+            </amp-youtube>              
+
+            <a class="button" href="https://breakoutgames.com/book-at-lexington/">Play at Lexington</a>
+            <hr>
+        </div>                       
+
+        <div class="game-room">
+            <img class="icon" src="https://2m14st1h0x1b30k8dw3a8fxi-wpengine.netdna-ssl.com/wp-content/uploads/2016/05/hostage.svg" />
+            <h1>Hostage</h1>
+            <p>While on a flight, your plane is hijacked and you lose consciousness as the cabin depressurizes. Later you wake up and realize you're trapped, handcuffed to your friends, in the hijackers headquarters. Can you lose your handcuffs, find out the details behind their hijacking and escape before time's up?</p>
+
+            <amp-youtube width="480"
+              height="270"
+              layout="responsive"
+              data-videoid="uaaUv7PCjnc">
+            </amp-youtube>              
+
+            <a class="button" href="https://breakoutgames.com/book-at-lexington/">Play at Lexington</a>
+            <hr>
+        </div>
+    </section>         
+
+    <section class="location">   
+        <h1>Lexington Locations</h1>
+        <h3>Breakout Lexington</h3>
+        <p>306 N Ashland Ave<br/>
+        Lexington, KY 40502</p>
+        <p><a href="tel:859-904-9090">Phone: 859-904-9090</a></p>
+        <ul>
+            <li><a href="https://checkout.breakoutgames.com/listing?location=3154P9J9J71491A95C747">View Game Times</a></li>
+            <li><a href="https://goo.gl/maps/kRkKo7konC12" target="_blank">Get Directions</a></li>
+            <li><a data-toggle="modal" data-target="#locNewsletterModal_357" data-dismiss="modal">Sign Up for Updates</a></li>
+        </ul>
+        <div id="map" class="m-map embed-responsive embed-responsive-1by1" data-map="[{&quot;location&quot;:&quot;Lexington&quot;,&quot;latitude&quot;:&quot;38.038902&quot;,&quot;longitude&quot;:&quot;-84.478457&quot;}]"></div>
+    </section>         
+
+    <footer>
+    <img src="https://2m14st1h0x1b30k8dw3a8fxi-wpengine.netdna-ssl.com/wp-content/uploads/2016/04/ftr-logo.svg"/>
+    <ul>
+        <li><a title="Team Building" href="https://breakoutgames.com/team-building/">Team Building</a></li>
+        <li><a title="Frequently Asked Questions" href="https://breakoutgames.com/faq-lexington/">Frequently Asked Questions</a></li>
+        <li><a title="Fill Out Waiver" href="https://breakoutgames.com/lexington-waiver/">Fill Out Waiver</a></li>
+        <li><a title="Gift Vouchers" href="https://breakoutgames.com/lexington-voucher/">Gift Vouchers</a></li>
+        <li><a title="Blog - Into the Maze" href="https://breakoutgames.com/blog/">Blog &#8211; Into the Maze</a></li>
+        <li><a title="Join our Team" href="https://breakoutgames.com/join-our-lexington-team/">Join our Team</a></li>
+        <li><a title="Contact Us" href="https://breakoutgames.com/contact-us/">Contact Us</a></li>
+        <li><a title="What Are Escape Rooms" href="https://breakoutgames.com/escape-rooms/">What are escape rooms?</a></li>
+    </ul>        
+    <p>Follow Us!</p>
+    <a target="_blank" href="https://www.facebook.com/breakoutgameslexington"><i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i></a>
+    <a target="_blank" href="https://www.instagram.com/breakoutlexington/"><i class="fa fa-instagram fa-3x" aria-hidden="true"></i></a>
+    <a target="_blank" href="https://twitter.com/breakoutlex"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>
+    <i class="fa fa-copyright icon-on-left"></i>copyright Breakout, 2017.</div>
+    <div id="fb-root"></div>
+    </footer>                
+                        
+    
+  </body>
 </html>
