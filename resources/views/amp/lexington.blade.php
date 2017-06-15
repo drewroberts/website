8ea03013742bb039c84d4efd5cf4f22d2266b60d
DrewRoberts <drew@roberts.email>
2017-06-15 12:49:27 -0500

    Edited Lex

--- resources/views/amp/lexington.blade.php
+++ resources/views/amp/lexington.blade.php
@@ -13,15 +13,17 @@
 <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 </head>
 <body>
+<script async custom-element="amp-youtube" src="https://cdn.ampproject.org/v0/amp-youtube-0.1.js"></script>
 <amp-img src="http://breakoutstaging.mediocrecreative.com/breakout/wp-content/themes/fm-framework/images/location_header_background_3.jpg"
   width="1200"
   height="536"
   layout="responsive"></amp-img>
 <h1>Lexington</h1>
-<script async custom-element="amp-youtube" src="https://cdn.ampproject.org/v0/amp-youtube-0.1.js"></script>
+<div style="max-width: 480;">
 <amp-youtube width="480"
   height="270"
-  layout="fixed"
+  layout="responsive"
   data-videoid="uaaUv7PCjnc">
 </amp-youtube>
+</div>
 </html>
