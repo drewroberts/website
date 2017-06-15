a3d85e19a91c6e13f0c8b53626530c86851bc219
DrewRoberts <drew@roberts.email>
2017-06-15 11:39:55 -0500

    Edited Web Routes

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('drew', function () {
+    return view('welcome');
+});
+
 Auth::routes();
 
 Route::get('lexington', function () {
