1867f26b114875ed77e8c247f6f7b767152e6375
DrewRoberts <drew@roberts.email>
2017-09-16 16:48:25 -0400

    Update Vue

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -23,12 +23,6 @@ if (token) {
 }
 
 /**
- * We'll load the Veutify library
- */
-
-window.vuetify = require('vuetify');
-
-/**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
