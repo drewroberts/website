7b4aaa32c2447c5bae8a3c99524c013f9572eab7
DrewRoberts <drew@roberts.email>
2017-09-17 22:10:43 -0400

    Update Vue

---
+++ resources/views/app.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Drew Roberts</title>
+
+        <!-- Structured Data -->
+        <script type="application/ld+json">
+        {
+          "@context": "http://schema.org",
+          "@type": "Person",
+          "name": "Drew Roberts",
+          "url": "https://DrewRoberts.com",
+          "sameAs": [
+            "https://www.facebook.com/DrewRoberts",
+            "https://twitter.com/DrewRoberts",
+            "https://www.linkedin.com/in/DrewRoberts",
+            "https://github.com/DrewRoberts",
+            "https://plus.google.com/+DrewRoberts"
+          ]
+        }
+        </script>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <div id="app">
+            <notification></notification>
+        </div>
+
+        <script src="/js/app.js"></script>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/app', function () {
+    return view('app');
+});
