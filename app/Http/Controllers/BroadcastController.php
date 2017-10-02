89565e12100e5af73f36a3f7d66f1931bd84c06e
DrewRoberts <drew@roberts.email>
2017-10-02 11:41:25 -0400

    Edit Migrations

---
+++ app/Http/Controllers/BroadcastController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Broadcast;
+use Illuminate\Http\Request;
+
+class BroadcastController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Broadcast  $broadcast
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Broadcast $broadcast)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Broadcast  $broadcast
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Broadcast $broadcast)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Broadcast  $broadcast
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Broadcast $broadcast)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Broadcast  $broadcast
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Broadcast $broadcast)
+    {
+        //
+    }
+}
