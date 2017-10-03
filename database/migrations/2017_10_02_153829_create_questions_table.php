393f63609a7fe12e5474a4348cbd39c108b0a0de
DrewRoberts <drew@roberts.email>
2017-10-03 10:15:33 -0400

    Edit Migrations

--- database/migrations/2017_10_02_153829_create_questions_table.php
+++ database/migrations/2017_10_02_153829_create_questions_table.php
@@ -15,7 +15,19 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('quiz_id')->index();
+            $table->tinyInteger('participants_blocked', 2)->unsigned()->default(1); // Use this to order the questions on the quiz. 1-99
+            $table->string('text')->nullable(); // The questions text. For now, limit to 255 characters
+            $table->unsignedInteger('created_by'); // Blocked_by
+            $table->unsignedInteger('updated_by');
             $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('questions', function($table) {
+            $table->foreign('quiz_id')->references('id')->on('quizzes')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('restrict')->onUpdate('cascade');
         });
     }
 
@@ -26,6 +38,14 @@ class CreateQuestionsTable extends Migration
      */
     public function down()
     {
+        Schema::table('questions', function ($table) {
+            $table->dropForeign(['quiz_id']);
+            $table->dropForeign(['created_by']);
+            $table->dropForeign(['updated_by']);
+        });
+
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('questions');
+        Schema::enableForeignKeyConstraints();
     }
 }
