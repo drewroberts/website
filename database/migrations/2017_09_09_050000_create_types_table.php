1e979232913a2851663b55ef611512f74cbdeb3c
DrewRoberts <drew@roberts.email>
2017-10-08 21:17:51 -0400

    Edit Migrations

--- database/migrations/2017_09_09_050000_create_types_table.php
+++ database/migrations/2017_09_09_050000_create_types_table.php
@@ -35,6 +35,8 @@ class CreateTypesTable extends Migration
      */
     public function down()
     {
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('types');
+        Schema::enableForeignKeyConstraints();
     }
 }
