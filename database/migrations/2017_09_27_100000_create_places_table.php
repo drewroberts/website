ae494c00480b100dd493114e2f7e889a49468a6e
DrewRoberts <drew@roberts.email>
2017-11-02 13:49:39 -0400

    Update Places & Products

--- database/migrations/2017_09_27_100000_create_places_table.php
+++ database/migrations/2017_09_27_100000_create_places_table.php
@@ -16,8 +16,12 @@ class CreatePlacesTable extends Migration
         Schema::create('places', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('market_id')->index(); // Use for URL structure. Example drewroberts.com/florida/orlando/corona or //florida/winter-park/barnies
+            $table->unsignedInteger('brand_id')->index(); // If brand has own page, then assign here
             $table->string('slug')->unique()->index(); // How location will display on the front end of website. No underscores, only lowercase letters and dashes.
-            $table->string('name')->unique(); // Name of place for display
+            $table->string('title')->unique(); // Name of place for display
+            $table->string('description'); // Really is an excerpt for social.
+            $table->unsignedInteger('image_id')->index(); // path to edited cover image for the recommendation
+            $table->unsignedInteger('video_id')->nullable(); // If video, then include the video id here.
             $table->date('first_visit')->default('2015-01-01'); // First time I went to place
             $table->date('opened')->default('2015-01-01'); // Estimated open date
             $table->date('closed')->nullable();
@@ -28,7 +32,6 @@ class CreatePlacesTable extends Migration
             $table->string('state', 2)->nullable();
             $table->string('zip', 5)->nullable();
             $table->string('phone', 25)->nullable();
-            $table->string('timezone')->nullable();
             $table->string('gmaps')->nullable()->unique(); // ID for location's Google Maps page
             $table->string('facebook')->nullable()->unique(); // Username for location's facebook page
             $table->string('instagram')->nullable()->unique(); // Username for location's instagram account
@@ -44,6 +47,9 @@ class CreatePlacesTable extends Migration
 
         Schema::table('places', function($table) {
             $table->foreign('market_id')->references('id')->on('markets')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('image_id')->references('id')->on('images')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('video_id')->references('id')->on('videos')->onDelete('restrict')->onUpdate('cascade');
             $table->foreign('type_id')->references('id')->on('types')->onDelete('restrict')->onUpdate('cascade');
             $table->foreign('created_by')->references('id')->on('users')->onDelete('restrict')->onUpdate('cascade');
             $table->foreign('updated_by')->references('id')->on('users')->onDelete('restrict')->onUpdate('cascade');
@@ -59,6 +65,9 @@ class CreatePlacesTable extends Migration
     {
         Schema::table('places', function ($table) {
             $table->dropForeign(['market_id']);
+            $table->dropForeign(['brand_id']);
+            $table->dropForeign(['image_id']);
+            $table->dropForeign(['video_id']);
             $table->dropForeign(['type_id']);
             $table->dropForeign(['created_by']);
             $table->dropForeign(['updated_by']);
--- database/migrations/2017_09_27_110000_create_products_table.php
+++ database/migrations/2017_09_27_110000_create_products_table.php
@@ -15,7 +15,32 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('brand_id')->index(); // If brand has own page, then assign here
+            $table->string('slug')->unique()->index(); // How location will display on the front end of website. No underscores, only lowercase letters and dashes.
+            $table->string('title')->unique(); // Name of place for display
+            $table->string('description'); // Really is an excerpt for social.
+            $table->unsignedInteger('image_id')->index(); // path to edited cover image for the recommendation
+            $table->unsignedInteger('video_id')->nullable(); // If video, then include the video id here.
+            $table->date('launched')->nullable(); // Estimated date when product first available.
+            $table->date('expired')->nullable();
+            $table->unsignedInteger('type_id')->nullable(); // Use for primary grouping of products by types. Can use other categories as well
+            $table->string('facebook')->nullable()->unique(); // Username if product has own facebook page
+            $table->string('instagram')->nullable()->unique(); // Username if product has own instagram account
+            $table->string('twitter')->nullable()->unique(); // Username if product has own twitter account
+            $table->string('youtube')->nullable()->unique(); // Username if product has own YouTube channel
+            $table->unsignedInteger('created_by')->default(1);
+            $table->unsignedInteger('updated_by')->default(1);
             $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('places', function($table) {
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('image_id')->references('id')->on('images')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('video_id')->references('id')->on('videos')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('restrict')->onUpdate('cascade');
         });
     }
 
@@ -26,6 +51,17 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::table('places', function ($table) {
+            $table->dropForeign(['brand_id']);
+            $table->dropForeign(['image_id']);
+            $table->dropForeign(['video_id']);
+            $table->dropForeign(['type_id']);
+            $table->dropForeign(['created_by']);
+            $table->dropForeign(['updated_by']);
+        });
+
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('places');
+        Schema::enableForeignKeyConstraints();
     }
 }
