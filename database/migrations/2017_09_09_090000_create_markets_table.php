a846442080c89aee4ae7094390a1b5dfa966718d
DrewRoberts <drew@roberts.email>
2017-11-06 11:52:29 -0500

    Add Markets

---
+++ database/migrations/2017_09_09_090000_create_markets_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMarketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('markets', function (Blueprint $table) { // Regions/cities for places I visit.
+            $table->increments('id');
+            $table->string('slug')->unique()->index();
+            $table->string('name')->unique(); // Don't allow spaces. Letter & underscores only
+            $table->string('title')->unique(); // Market Title for Display
+            $table->string('state', 2); // Primary state for the market. Can span multiple states, but this is the main one.
+            $table->string('state_slug')index(); // Do I need this?
+            $table->unsignedInteger('created_by');
+            $table->unsignedInteger('updated_by');
+            $table->timestamps();
+        });
+
+        Schema::table('markets', function($table) {
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('restrict')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('markets', function ($table) {
+            $table->dropForeign(['created_by']);
+            $table->dropForeign(['updated_by']);
+        });
+
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('markets');
+        Schema::enableForeignKeyConstraints();
+    }
+}
