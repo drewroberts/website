e48eb3adf9da8d1f4bd380830ad5388b19554255
DrewRoberts <drew@roberts.email>
2017-11-18 15:56:22 -0500

    Edit ZIP

--- database/migrations/2017_09_09_095000_create_zips_table.php
+++ database/migrations/2017_09_09_095000_create_zips_table.php
@@ -21,7 +21,7 @@ class CreateZipsTable extends Migration
             $table->string('city_alternate')->nullable();
             $table->string('city_alternate_2')->nullable();
             $table->string('city_alternate_3')->nullable();
-            $table->string('city_alternate_4')->nullable(); // Is there any ZIP Code with more than 5 acceptable city names?
+            $table->string('city_alternate_4')->nullable(); // Limit ZIP Codes to 5 acceptable city names.
             $table->string('county')->nullable();
             $table->string('timezone')->nullable();
             $table->decimal('latitude', 4, 2)->nullable();
