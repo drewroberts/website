3efb25f08c99808381927c75500d92ddfcbe8d65
DrewRoberts <drew@roberts.email>
2017-11-20 12:27:32 -0500

    Update Topic Migration

--- database/migrations/2017_09_25_100000_create_topics_table.php
+++ database/migrations/2017_09_25_100000_create_topics_table.php
@@ -16,13 +16,14 @@ class CreateTopicsTable extends Migration
         Schema::create('topics', function (Blueprint $table) { // Website sections, topics for content
             $table->increments('id');
             $table->string('slug')->unique()->index();
-            $table->unsignedInteger('type_id')->index(); // Allows groupings for positions in on homepage, features, treatment of topic, etc.
-            // Need a parent topic relationship
+            $table->unsignedInteger('type_id')->nullable()->index(); // Allows groupings for positions in on homepage, features, treatment of topic, etc.
+            $table->unsignedInteger('parent_id')->nullable()->index(); // ID of the parent topic relationship. ID's below 20.
             $table->string('title')->unique();
-            $table->string('description');
-            $table->string('excerpt'); // Longer description for homepage and parent topic pages.
-            $table->unsignedInteger('image_id')->nullable()->index(); // Featured image for social sharing.
-            $table->unsignedInteger('icon_id')->nullable(); // Topics have icons for menu navigation. Should we have small icon and heroicon?
+            $table->string('description')->nullable();
+            $table->string('excerpt')->nullable(); // Longer description for homepage and parent topic pages.
+            $table->unsignedInteger('image_id')->nullable(); // Featured image for social sharing. Typically just heroicon on a stylized color background.
+            $table->unsignedInteger('icon_id')->nullable(); // Topics have icons for menu navigation.
+            $table->unsignedInteger('heroicon_id')->nullable(); // Topics have heroicons that display when featured.
             $table->unsignedInteger('created_by')->default(1)->index();
             $table->unsignedInteger('updated_by')->default(1);
             $table->timestamps();
@@ -31,8 +32,10 @@ class CreateTopicsTable extends Migration
 
         Schema::table('topics', function($table) {
             $table->foreign('type_id')->references('id')->on('types')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('parent_id')->references('id')->on('topics')->onDelete('restrict')->onUpdate('cascade');
             $table->foreign('image_id')->references('id')->on('images')->onDelete('restrict')->onUpdate('cascade');
             $table->foreign('icon_id')->references('id')->on('images')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('heroicon_id')->references('id')->on('images')->onDelete('restrict')->onUpdate('cascade');
             $table->foreign('created_by')->references('id')->on('users')->onDelete('restrict')->onUpdate('cascade');
             $table->foreign('updated_by')->references('id')->on('users')->onDelete('restrict')->onUpdate('cascade');
         });
@@ -47,8 +50,10 @@ class CreateTopicsTable extends Migration
     {
         Schema::table('topics', function ($table) {
             $table->dropForeign(['type_id']);
+            $table->dropForeign(['parent_id']);
             $table->dropForeign(['image_id']);
             $table->dropForeign(['icon_id']);
+            $table->dropForeign(['heroicon_id']);
             $table->dropForeign(['created_by']);
             $table->dropForeign(['updated_by']);
         });
