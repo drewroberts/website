ab3740923fec9a7bbefb908b5b9677fce42fc9c1
DrewRoberts <drew@roberts.email>
2017-11-21 11:48:00 -0500

    Update Topic Migration

--- database/migrations/2017_09_25_100000_create_topics_table.php
+++ database/migrations/2017_09_25_100000_create_topics_table.php
@@ -20,8 +20,8 @@ class CreateTopicsTable extends Migration
             $table->unsignedInteger('parent_id')->nullable()->index(); // ID of the parent topic relationship. ID's below 20.
             $table->string('title')->unique();
             $table->string('description')->nullable();
-            $table->string('note')->nullable(); // Just for internal reference purposes only, not displayed on website.
             $table->mediumText('excerpt')->nullable(); // Longer introduction for homepage and parent topic pages.
+            $table->string('note')->nullable(); // Just for internal reference purposes only, not displayed on website.
             $table->unsignedInteger('image_id')->nullable(); // Featured image for social sharing. Typically just heroicon on a stylized color background.
             $table->unsignedInteger('icon_id')->nullable(); // Topics have icons for menu navigation.
             $table->unsignedInteger('heroicon_id')->nullable(); // Topics have heroicons that display when featured.
