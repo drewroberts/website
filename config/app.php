c4ed82e8e37627d5618cb3f3a5b3c8b86256e662
DrewRoberts <drew@roberts.email>
2018-01-26 09:46:02 -0500

    Update App Config File

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Drew Roberts'),
 
     /*
     |--------------------------------------------------------------------------
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://drewroberts.com'),
 
     /*
     |--------------------------------------------------------------------------
