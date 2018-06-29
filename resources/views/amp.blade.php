0d86810767424b077e63c530436be06fd38dd131
DrewRoberts <drew@roberts.email>
2018-06-29 00:17:53 -0400

    Update AMP Template

--- resources/views/amp.blade.php
+++ resources/views/amp.blade.php
@@ -633,7 +633,7 @@ amp-carousel > amp-img > img {
 .primary-footer__social a.facebook{background-image:url(https://theundefeated.com/wp-content/themes/espn-theundefeated/assets/images/symbol-facebook-black.svg)}
 .primary-footer__social a.twitter{background-image:url(https://theundefeated.com/wp-content/themes/espn-theundefeated/assets/images/symbol-twitter-black.svg)}
 .amp-wp-article{font-family:"ESPN Acta",serif;margin:0;max-width:none}
-.story-header{background:#222 url(https://theundefeated.com/wp-content/themes/espn-theundefeated/assets/images/mine-shaft-bg.svg);color:#fff;background-size:6px;padding:20px}
+.story-header{background:#222 url(https://theundefeated.com/wp-content/themes/espn-theundefeated/assets/images/mine-shaft-bg.svg);color:#fff;background-size:6px;padding:(75px,20px,20px,20px)}
 .story-header>.term{color:#333334;background-color:#f7cf4f;background-position:0 0;background-size:100% 100%;background-image:svg-url('');display:inline-block;font:normal 400 12px/1 BentonSansComp,sans-serif;letter-spacing:1px;margin-bottom:10px;padding:5px 9px 4px;text-transform:uppercase;margin-bottom:15px}
 .story-header>.link{display:block;overflow:visible;line-height:25px}
 .story-header .heading,.story-header .subheading{display:block;overflow:visible;vertical-align:baseline;    max-width: 1180px;     margin: 0 auto; }
@@ -889,7 +889,7 @@ a.podcast::before{background:url(https://theundefeated.com/wp-content/themes/esp
 <article class="amp-wp-article story">
 
 <div class="story-header">
-	<a href="https://theundefeated.com/tag/college-football/" class="term " name="">College Football</a>
+	<a href="https://theundefeated.com/tag/college-football/" class="term" name="">College Football</a>
 	<h1 class="heading entry-title"><span>FSU coach Willie Taggart buys lunch for local football campers, wins the day</span></h1>
 			<p class="subheading">The Seminoles coach filled stomachs, and filled hearts even more</p>
 	</div>
