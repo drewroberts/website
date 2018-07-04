27b2fbd03d39b28d0a428c91b88c2ef8884f9988
DrewRoberts <drew@roberts.email>
2018-07-04 10:52:10 -0400

    Update AMP Template

--- resources/views/amp.blade.php
+++ resources/views/amp.blade.php
@@ -639,13 +639,32 @@ img,picture{margin:0;max-width:100%;}
 }
 
 .amp-carousel-button {
-
+	transition: height .25s;
+  visibility: hidden;
+  position: absolute;
+  top: 0;
+  width: 30px;
+  padding: 9px;
+  z-index: 2;
+  border: 0;
+  border-radius: 0;
+  background: #000;
+  border-color: #fff;
 }
 
 .amp-carousel-button-prev {
   left: 16px;
-  background-image: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13"><path d="M0 11.63L1.393 13 8 6.5 1.393 0 0 1.37 5.214 6.5z" fill="#fff" /></svg>');
+  background-image: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13"><path d="M8 11.63L6.607 13 0 6.5 6.607 0 8 1.37 2.786 6.5z" fill="#fff" /></svg>');
+	background-size: 8px 13px;
 }
+
+.amp-carousel-button-next {
+    right: 16px;
+		background-image: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13"><path d="M0 11.63L1.393 13 8 6.5 1.393 0 0 1.37 5.214 6.5z" fill="#fff" /></svg>');
+		background-size: 8px 13px;
+}
+
+
 
 
 </style>
