9d76d0a6da13ca54c2be35c185f23ab8b235f285
DrewRoberts <drew@roberts.email>
2018-08-05 20:19:07 -0400

    Edit Migrations

--- database/migrations/2017_09_12_130000_create_tag_tables.php
+++ database/migrations/2017_09_12_130000_create_tag_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTagsTable extends Migration
+class CreateTagTable extends Migration
 {
     /**
      * Run the migrations.
