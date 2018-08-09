fd53ddb246393b6841f2a1b514cffff15ab3f54b
DrewRoberts <drew@roberts.email>
2018-08-08 23:18:49 -0400

    Update Controllers

--- app/Http/Controllers/Auth/ProfileController.php
+++ app/Http/Controllers/Auth/ProfileController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
 use App\Profile;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ProfileController extends Controller
 {
--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
 use App\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
