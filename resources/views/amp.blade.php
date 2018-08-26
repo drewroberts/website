c23534feba2492944c8ff8f15012cfcdf6cdebb4
DrewRoberts <drew@roberts.email>
2018-08-25 21:47:36 -0400

    Update Sidebar

--- resources/views/amp.blade.php
+++ resources/views/amp.blade.php
@@ -1217,8 +1217,8 @@ button,input{font-family:HelveticaNeueArabic,Lora,Georgia,TazuganeGothic,serif;}
 					{{csrf_field()}}
 					<div class="newsletter p-4-xs p-5-m dsp-flex-xs flex-wrap col-12-xs">
 						<div class="newsletter__text col-12-xs">
-							<h2 class="newsletter__heading hed-l m-b-3-xs">Newsletters are the new newsletters.</h2>
-							<p class="newsletter__subheading hed-xs">Sign up for codinging tips and personalized insights, delivered weekly.</p>
+							<h2 class="newsletter__heading hed-l m-b-3-xs">Get the best Newsletters for Web Developers.</h2>
+							<p class="newsletter__subheading hed-xs">Sign up for coding tips and personalized insights, delivered weekly.</p>
 						</div>
 						<div class="newsletter__input__container__top dsp-flex-xs col-12-xs grd-algn-item-flex-end-xs">
 							<div class="newsletter__input dsp-flex-xs">
