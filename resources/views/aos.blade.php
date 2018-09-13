9dbf7b694fef6a126716bbef06378d5baa6ce1bb
DrewRoberts <github@drewroberts.com>
2018-09-13 13:00:00 +0100

    Update AOS Contact Form

--- resources/views/aos.blade.php
+++ resources/views/aos.blade.php
@@ -1168,6 +1168,7 @@ div.button:hover {
 *,::after,::before{box-sizing:border-box;}
 input{margin:0;font-family:inherit;font-size:inherit;line-height:inherit;}
 input{overflow:visible;}
+label{display:inline-block;margin-bottom:.5rem;}
 .form-control{display:block;width:100%;padding:.375rem .75rem;font-size:1rem;line-height:1.5;color:#495057;background-color:#fff;background-clip:padding-box;border:1px solid #ced4da;border-radius:.25rem;transition:border-color .15s ease-in-out,box-shadow .15s ease-in-out;}
 .form-control::-ms-expand{background-color:transparent;border:0;}
 .form-control:focus{color:#495057;background-color:#fff;border-color:#80bdff;outline:0;box-shadow:0 0 0 .2rem rgba(0,123,255,.25);}
