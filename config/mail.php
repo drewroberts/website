f26e0c85a60fe62da83342fd46ec5313e2cbb362
Drew Roberts <github@drewroberts.com>
2024-02-19 21:42:03 -0500

    Add postmark

--- config/mail.php
+++ config/mail.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'smtp'),
+    'default' => env('MAIL_MAILER', 'postmark'),
 
     /*
     |--------------------------------------------------------------------------
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'mail@drewroberts.com'),
+        'name' => env('MAIL_FROM_NAME', 'Drew Roberts'),
     ],
 
     /*
