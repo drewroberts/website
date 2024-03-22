4eeb5acb33128de7f6986b71d5200bbe553f715a
PND Design <ipnddev@gmail.com>
2024-03-22 11:36:01 -0500

    newsletter widget

--- resources/views/amp/page-content/newsletter-widget.blade.php
+++ resources/views/amp/page-content/newsletter-widget.blade.php
@@ -1,27 +1,23 @@
-<div class="sidebar">
-    <form method="post" class="p2 i-amphtml-form" action-xhr="" target="_top" custom-validation-reporting="show-first-on-submit" novalidate="">
+<div class="bg-gray-100">
+    <form method="post" class="p-5">
         <input type="hidden" name="_token" value="3wSBZm9GM1F2AcMobOzOXHfYvbFD05QFdbyz5zOi">
-        <div class="newsletter p-4-xs p-5-m dsp-flex-xs flex-wrap col-12-xs">
-            <div class="newsletter__text col-12-xs">
-                <h2 class="newsletter__heading hed-l m-b-3-xs">Get the #1 Newsletter for Programmers.</h2>
-                <p class="newsletter__subheading hed-xs">Sign up for coding tips and personalized insights, delivered weekly.</p>
+        <div class="newsletter">
+            <div class="mb-4">
+                <h2 class="font-bold text-black text-4xl mb-4">Get the #1 Newsletter for Programmers.</h2>
+                <p class="font-bold uppercase text-black">Sign up for coding tips and personalized insights, delivered weekly.</p>
             </div>
-            <div class="newsletter__input__container__top dsp-flex-xs col-12-xs grd-algn-item-flex-end-xs">
-                <div class="newsletter__input dsp-flex-xs">
-                    <input id="email" type="email" required="" placeholder="Email Address" class="input-text" value="">
-                </div>
+            <div class="mb-4">
+                <input id="email" type="email" required="" placeholder="Email Address" class="bg-white text-black p-2 rounded w-full">
+            </div>
+            <div class="mb-4">
+                <input id="name" type="text" required="" placeholder="Name" class="bg-white text-black p-2 rounded w-full">
+            </div>
+            <div>
+                <input type="submit" value="Subscribe" class="text-white bg-black uppercase px-6 py-2 rounded">
             </div>
             <span visible-when-invalid="valueMissing" validation-for="show-first-on-submit-name"></span>
-
-            <div class="newsletter__input__container dsp-flex-xs col-12-xs grd-algn-item-flex-end-xs">
-                <div class="newsletter__input dsp-flex-xs">
-                    <input id="name" type="text" required="" placeholder="Name" class="input-text" value="">
-                </div>
-                <input type="submit" value="Subscribe" class="newsletter__submit__button btn btn-branded dsp-block-xs">
-            </div>
             <span visible-when-invalid="valueMissing" validation-for="show-first-on-submit-email"></span>
             <span visible-when-invalid="typeMismatch" validation-for="show-first-on-submit-email"></span>
-
             <div submit-success="">
                 <template type="amp-mustache">
                     Success! Thanks "Demo" for trying the
@@ -37,5 +33,4 @@
             </div>
         </div>
     </form>
-
 </div>
