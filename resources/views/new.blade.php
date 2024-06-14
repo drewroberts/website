39ecad9e72043a20a3295ebccf18e31d19de4aa8
Drew Roberts <github@drewroberts.com>
2024-06-14 15:20:37 -0400

    Remove tailwind cdn

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-        <script src="https://cdn.tailwindcss.com"></script>
+
         <script>
             tailwind.config = {
             theme: {
@@ -53,6 +53,8 @@
 
         <meta property="og:description" content="">
         <meta property="og:image" content="">
+
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
         <style>
             .logo {
